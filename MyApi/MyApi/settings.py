0660a041b10ea74824eda9ea2b99ec0be4dcf136
Ashish Kapoor <swiftobjc@gmail.com>
2024-03-13 17:10:03 +0530

    serializers

--- MyApi/MyApi/settings.py
+++ MyApi/MyApi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cvsiqfynoxf#^3wyvv^=8x*7xr*+@6fu9z87&9ii--k_r@#!!+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
