2656dbc0fa3ad8a9c4f2b7ca8924a561c74dd32e
Ashish Kapoor <swiftobjc@gmail.com>
2024-03-14 10:34:33 +0530

    add comment

--- MyApi/backend/serializers.py
+++ MyApi/backend/serializers.py
@@ -3,6 +3,8 @@ from .models import Article
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    # sets the slug field as not required
+    # and through signal pre_save it gets auto generated from title field
     slug = serializers.SlugField(read_only=True)
     # hides the field below from /api form
     author = serializers.StringRelatedField()
